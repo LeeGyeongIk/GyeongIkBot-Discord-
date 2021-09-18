e5a3fb8cfb6cffc7ea5d7cdd3d9b3b4c67f48628
LeeGyeongIk <73739372+LeeGyeongIk@users.noreply.github.com>
2021-09-18 10:51:32 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -9,6 +9,7 @@ from discord.utils import get
 from discord import FFmpegPCMAudio
 import asyncio
 import time
+import os
 
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
@@ -459,4 +460,5 @@ async def on_reaction_add(reaction, users):
             elif str(reaction.emoji) == '\U0001F4DD':
                 await reaction.message.channel.send("플레이리스트가 나오면 생길 기능이에요.")
 
-bot.run('ODg4MzI1MjQ2OTU0OTMwMTc3.YURDWw.1dmUsM9SplDTAaMg9YcfCucw8QQ')
+access_token = os.environ["BOT_TOKEN"]
+bot.run(access_token)
