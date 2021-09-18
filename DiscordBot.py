9afe6e1074dcbdcde0f2b78b7d413d7637cc78cb
LeeGyeongIk <73739372+LeeGyeongIk@users.noreply.github.com>
2021-09-18 10:41:44 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -459,4 +459,4 @@ async def on_reaction_add(reaction, users):
             elif str(reaction.emoji) == '\U0001F4DD':
                 await reaction.message.channel.send("플레이리스트가 나오면 생길 기능이에요.")
 
-bot.run('ODg4MzI1MjQ2OTU0OTMwMTc3.YURDWw.5aYg6VQl_uMI_genZboXv0DcVAI')
+bot.run('ODg4MzI1MjQ2OTU0OTMwMTc3.YURDWw.6qf9gHKm5zUAe-dh1FKabvhnCGA')
